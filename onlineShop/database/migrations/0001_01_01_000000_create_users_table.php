ab56d4f4709f8ad82b5ff78b77bf8ef0393eb1fb
Seyyed_Amirreza <amir9bahoosh@gmail.com>
2024-11-11 18:33:21 +0330

    first commit

---
+++ onlineShop/database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->char('firstName')->length(30);
+            $table->char('lastName')->length(30);
+            $table->string('userName')->length(20)->unique();
+            $table->timestamp('lastLogin');
+            $table->string('password');
+            $table->char('permissions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ onlineShop/database/migrations/2024_10_09_141120_create_factor_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factor', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->timestamp('createdAt',precision: 0);
+            $table->timestamp('paidAt',precision: 0);
+            $table->integer('total')->length(10);
+            $table->text('contents');
+            $table->char('status');
+            $table->char('userName')->length(20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('factor');
+    }
+};
