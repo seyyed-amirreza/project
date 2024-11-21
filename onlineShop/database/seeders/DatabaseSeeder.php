e8215f04b26356f9baea5e0aef7a7c66ff94b72a
Seyyed_Amirreza <amir9bahoosh@gmail.com>
2024-11-21 14:44:36 +0330

    created models

--- onlineShop/database/seeders/DatabaseSeeder.php
+++ onlineShop/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Users;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Users::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+        Users::factory()->create([
+            'name' => 'Test Users',
             'email' => 'test@example.com',
         ]);
     }
